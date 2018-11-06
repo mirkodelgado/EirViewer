66e0bacc998021fd165c8c246cf99c6ee9bd07c2
mirkodelgado <mirkodelgado@comcast.net>
2018-11-06 09:50:28 -0500

    Initial Commit

---
+++ EirViewer.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using EirViewer.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EirViewer.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options)
+        {
+            
+        }
+
+        public  DbSet<Value> Values { get; set; }
+    }
+}
